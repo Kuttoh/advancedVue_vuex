1faf879cc2832e15643b53cf34b5fb1d9cc66a50
Kuttoh <kuttohisaac@gmail.com>
2019-06-19 18:48:07 +0300

    initial commit

---
+++ src/components/ContactDetail.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="contact-detail">
+        <h1>
+            Contact Details
+        </h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ContactDetail"
+    }
+</script>
+
+<style scoped>
+
+</style>
